fbcf121c05de549a80fe14c747053481f5c8c9ae
Lothar Maisenbacher <lothar.maisenbacher@berkeley.edu>
2023-04-06 21:00:45 -0700

    ...

--- web-interface/web-interface.py
+++ web-interface/web-interface.py
@@ -408,24 +408,24 @@ def set_sg_enabled():
         if retval != 0:
             LOG.error("Failed to disable signal generator. Error code: %s", ERROR_CODES[retval])
 
-# @route("/_set_sg_poffset_enabled", method="POST")
-# def set_sg_poffset_enabled():
-#     """Handle POST request for enabling permanent offset of the signal generator.
+@route("/_set_sg_poffset_enabled", method="POST")
+def set_sg_poffset_enabled():
+    """Handle POST request for enabling permanent offset of the signal generator.
 
-#     Accepted POST parameters:
-#     :output: the output channel to adjust
-#     :offset_enabled: true if permanent offset of signal generator enabled, false if not
-#     """
-#     offset_enabled = request.params.get("offset_enabled", 0) == "true"
-#     output = request.params.get("output", 1, type=int)
-#     if offset_enabled:
-#         retval = RP_LIB.rp_GenPOffsetEnable(output)
-#         if retval != 0:
-#             LOG.error("Failed to enable permanent offset of signal generator. Error code: %s", ERROR_CODES[retval])
-#     else:
-#         retval = RP_LIB.rp_GenPOffsetDisable(output)
-#         if retval != 0:
-#             LOG.error("Failed to disable permanent offset of signal generator. Error code: %s", ERROR_CODES[retval])            
+    Accepted POST parameters:
+    :output: the output channel to adjust
+    :offset_enabled: true if permanent offset of signal generator enabled, false if not
+    """
+    offset_enabled = request.params.get("offset_enabled", 0) == "true"
+    output = request.params.get("output", 1, type=int)
+    if offset_enabled:
+        retval = RP_LIB.rp_GenPOffsetEnable(output)
+        if retval != 0:
+            LOG.error("Failed to enable permanent offset of signal generator. Error code: %s", ERROR_CODES[retval])
+    else:
+        retval = RP_LIB.rp_GenPOffsetDisable(output)
+        if retval != 0:
+            LOG.error("Failed to disable permanent offset of signal generator. Error code: %s", ERROR_CODES[retval])            
 
 @route("/_save_parameters", method="POST")
 def save_parameters():
