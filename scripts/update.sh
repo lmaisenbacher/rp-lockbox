e6f327a30d79f207aa328dc301a180c7272cef11
Lothar Maisenbacher <lothar.maisenbacher@mpq.mpg.de>
2023-05-02 11:31:28 -0700

    Added update script.

---
+++ scripts/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+systemctl stop lockbox-web-interface
+systemctl stop lockbox
+rw
+cp fpga/prj/lockbox/out/red_pitaya.bit /opt/redpitaya/fpga/lockbox.bit
+cp api/lib/liblockbox.so /opt/redpitaya/lib/
+cp scpi-server/lockbox-server /opt/redpitaya/bin/
+cp -r web-interface /opt/redpitaya/
+cp systemd/lockbox.service /etc/systemd/system/
+cp systemd/lockbox-web-interface.service /etc/systemd/system/
+ro
+systemctl start lockbox
+systemctl start lockbox-web-interface
