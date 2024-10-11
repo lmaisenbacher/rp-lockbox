8307389ae5815e9a1b9735f4838dc726550fc629
Noeli Paz Soldan <noeli.pazsoldan@czbiohub.org>
2024-10-11 15:20:38 -0700

    Fix for OS 2.0

--- scripts/install_makeOSv2.sh
+++ scripts/install_makeOSv2.sh
@@ -4,6 +4,6 @@ cp fpga/prj/lockbox/out/red_pitaya.bit.bin /opt/redpitaya/fpga/lockbox.bit.bin
 cp api/lib/liblockbox.so /opt/redpitaya/lib/
 cp scpi-server/lockbox-server /opt/redpitaya/bin/
 cp -r web-interface /opt/redpitaya/
-cp systemd/lockbox.service /etc/systemd/system/
+cp systemd/lockboxOSv2.service /etc/systemd/system/
 cp systemd/lockbox-web-interface.service /etc/systemd/system/
 ro
