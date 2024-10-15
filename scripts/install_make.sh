6994308ba8fbfebdb1e09c6b5b5f240905944e13
Noeli Paz Soldan <noeli.pazsoldan@czbiohub.org>
2024-10-15 09:52:30 -0700

    fix incorrect file type

--- scripts/install_make.sh
+++ scripts/install_make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rw
-cp fpga/prj/lockbox/out/red_pitaya.bit.bin /opt/redpitaya/fpga/lockbox.bit.bin
+cp fpga/prj/lockbox/out/red_pitaya.bit /opt/redpitaya/fpga/lockbox.bit
 cp api/lib/liblockbox.so /opt/redpitaya/lib/
 cp scpi-server/lockbox-server /opt/redpitaya/bin/
 cp -r web-interface /opt/redpitaya/
